5371f940fa82170f464ef1900fbaedf4ffbad118
joaors <joao.rodrigo@live.com>
2019-08-20 21:27:31 -0300

    formularios

--- src/app/curso/curso.component.ts
+++ src/app/curso/curso.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-curso',
@@ -6,12 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./curso.component.css']
 })
 export class CursoComponent implements OnInit {
-
   cursoAtual: string = '';
+
+  curso: any = {
+    nome: null,
+    descricao: null,
+    email: null
+  }
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(formulario: NgForm) {
+    console.log(formulario);
+
+    console.log('---- valores do formulário');
+    console.log(formulario.form.value);
+
+    console.log('Valor do objeto')
+    console.log(this.curso);
   }
 
 }
